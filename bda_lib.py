175887aef8419bcddb232c767d66c5913d7927e2
Jędrzej <jerdek333@gmail.com>
2018-10-30 12:05:11 +0100

    Finished Loaddata

--- bda_lib.py
+++ bda_lib.py
@@ -36,10 +36,34 @@ def dataStatistics(data,stat):
 
     return results
 
+
 def dataLoad(filename):
     # Insert your code here
-    data=open(filename)
-    data = data.readlines()
-    rows=math.ceil(np.size(data))
-    data=np.reshape(data,[rows,1])
+    data=np.array([])
+    #3rd row posiible values
+    values=np.array([1,2,3,4])
+    with open(filename,'r') as f:
+        for line in f:
+            for word in line.split():
+                data=np.hstack((data,np.array(float(word))))
+    rows=math.ceil(np.size(data)/3)
+    data=np.reshape(data,[rows,3])
+    correctrows_number=0
+    for i in range(rows):
+        row=data[i,:]
+        errorcounter=0
+        if (row[0]>60 or row[0]<10):
+            errorcounter=errorcounter+1
+        if (row[1]<=0):
+            errorcounter=errorcounter+1
+        column3=values[values==row[2]]
+        if (np.size(column3)!=1):
+            errorcounter=errorcounter+1
+        if errorcounter==0:
+            correctrows_number=correctrows_number+1
+            if correctrows_number==1:
+                correctdata=row
+            elif correctrows_number>1:
+                correctdata=np.vstack((correctdata,row))
+    data=correctdata
     return data
