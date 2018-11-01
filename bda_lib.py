75adeb7cf417aac9a52c5649575cd3a89e5980b8
Jędrzej <jerdek333@gmail.com>
2018-11-01 15:25:25 +0100

    dataLoad txt  ;(
    data with filter

--- bda_lib.py
+++ bda_lib.py
@@ -38,32 +38,40 @@ def dataStatistics(data,stat):
 
 
 def dataLoad(filename):
-    # Insert your code here
-    data=np.array([])
-    #3rd row posiible values
-    values=np.array([1,2,3,4])
-    with open(filename,'r') as f:
-        for line in f:
+
+    matrix = np.array([]) # This variable will consist of data from txt
+
+    with open(filename,'r') as f: # opening file
+        for line in f: # changing file into an array containing values exept strings
             for word in line.split():
-                data=np.hstack((data,np.array(float(word))))
-    rows=math.ceil(np.size(data)/3)
-    data=np.reshape(data,[rows,3])
-    correctrows_number=0
-    for i in range(rows):
-        row=data[i,:]
-        errorcounter=0
-        if (row[0]>60 or row[0]<10):
-            errorcounter=errorcounter+1
-        if (row[1]<=0):
-            errorcounter=errorcounter+1
-        column3=values[values==row[2]]
-        if (np.size(column3)!=1):
-            errorcounter=errorcounter+1
-        if errorcounter==0:
-            correctrows_number=correctrows_number+1
-            if correctrows_number==1:
-                correctdata=row
-            elif correctrows_number>1:
-                correctdata=np.vstack((correctdata,row))
-    data=correctdata
+                matrix = np.hstack((matrix, np.array(float(word))))
+
+    rows = math.ceil(np.size(matrix)/3) # finding number of rows
+    matrix = np.reshape(matrix,[rows,3])
+    correct = 0 # number of correct rows
+
+    for i in range(rows): # checking statements for every row
+
+        row = matrix[i,:]
+        err=np.array([]) # this array will contain every type of error in line
+
+        if (row[0] > 60 or row[0] < 10): # Statement for temperature error
+            err = np.concatenate((err, ['Temperature']))
+
+        if (row[1] <= 0): # Statement for growth rate error
+            err=np.concatenate((err, ['Growth rate']))
+
+        if(row[2] < 1 or row[2] > 4 or ((row[2]).is_integer() == False)):
+            err = np.concatenate((err, ['Bacteria']))
+
+        if np.size(err) == 0:
+            correct += 1
+            if correct == 1:
+                data = row
+            elif correct > 1:
+                data = np.vstack((data,row))
+
+        else:
+            print("Error(s) in line {}: {}.".format(i+1, ", ".join(str(e) for e in err)) )
+
     return data
